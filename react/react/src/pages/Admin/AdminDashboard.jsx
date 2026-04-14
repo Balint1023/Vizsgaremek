a31bf6b4e41c3d841d4958eb8ea4fe1150607ce6
Csabii06 <nagycsabii06@gmail.com>
2026-04-14 10:35:04 +0200

    Reszponzív javítások
    Reszponzív navbar létrehozása

--- react/react/src/pages/Admin/AdminDashboard.jsx
+++ react/react/src/pages/Admin/AdminDashboard.jsx
@@ -1,22 +1,43 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink, useLocation } from "react-router-dom";
 import LogoutButton from "../../components/LogoutButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as Icon from 'react-bootstrap-icons';
 import "./AdminDashboard.css"
 
 const AdminDashboard = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setIsOpen(false);
+    }, [location]);
+
     return (
         <div className="admin-layout">
             <aside className={`admin-sidebar ${isOpen ? "open" : ""}`}>
                 <article>
                     <h1><Icon.Person color="white" style={{ marginRight: '8px' }} />Admin</h1>
                     <nav>
-                        <NavLink to="/admin/dashboard/kerdoivek" className={({ isActive }) => "menu-item" + (isActive ? " active" : "")} ><Icon.House color="white" style={{ marginRight: '8px', }} />Kérdőívek</NavLink>
-                        <NavLink to="/admin/dashboard/kerdesek"><Icon.QuestionSquare color="white" style={{ marginRight: '8px' }} />Kérdések</NavLink>
-                        <NavLink to="/admin/dashboard/eredmenyek"><Icon.Clipboard color="white" style={{ marginRight: '8px' }} />Eredmények</NavLink>
+                        <NavLink 
+                            to="/admin/dashboard/kerdoivek" 
+                            className={({ isActive }) => "menu-item" + (isActive ? " active" : "")}
+                        >
+                            <Icon.House color="white" style={{ marginRight: '8px' }} />
+                            Kérdőívek
+                        </NavLink>
+
+                        <NavLink to="/admin/dashboard/kerdesek">
+                            <Icon.QuestionSquare color="white" style={{ marginRight: '8px' }} />
+                            Kérdések
+                        </NavLink>
+
+                        <NavLink to="/admin/dashboard/eredmenyek">
+                            <Icon.Clipboard color="white" style={{ marginRight: '8px' }} />
+                            Eredmények
+                        </NavLink>
                     </nav>
                 </article>
+
                 <article className="buttonArticle">
                     <LogoutButton />
                 </article>
@@ -27,9 +48,6 @@ const AdminDashboard = () => {
                 <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
                     ☰
                 </button>
-                <header>
-                    <h1>Minőségbiztosítási kérdőív</h1>
-                </header>
                 <div className="content-inner">
                     <Outlet />
                 </div>
