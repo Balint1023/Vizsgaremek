cf08c69b26166166624f34a13ca95e7c634a4a19
Csabii06 <nagycsabii06@gmail.com>
2026-04-16 11:28:52 +0200

    Szöveg törlése
    Felesleges szöveg eltávolítása

--- react/react/src/pages/Admin/AdminSurveyControlPage.jsx
+++ react/react/src/pages/Admin/AdminSurveyControlPage.jsx
@@ -5,7 +5,6 @@ const AdminSurveyControlPage = () => {
     return (
         <div className="admin-page-content">
             <h2>Kérdőívek kezelése</h2>
-            <p>Itt tudod elindítani vagy lezárni a kitöltési időszakot.</p>
             <AdminSurveyControl />
         </div>
     );
